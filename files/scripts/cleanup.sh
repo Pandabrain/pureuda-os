5c9a00713e23412df99dac1a2a291adf96640429
Christoph Rechtlehner <24666261+Pandabrain@users.noreply.github.com>
2026-03-05 11:37:40 +0100

    remove unneeded cleanup entries

--- files/scripts/cleanup.sh
+++ files/scripts/cleanup.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-rm -f /usr/share/applications/byobu.desktop
-rm -f /usr/share/applications/nvim.desktop
+#rm -f /usr/share/applications/byobu.desktop #not installed
+#rm -f /usr/share/applications/nvim.desktop #neovim not installed
 rm -f /usr/share/applications/btop.desktop
-rm -f /usr/share/applications/Helix.desktop
-rm -f /usr/share/applications/helix.desktop
+#rm -f /usr/share/applications/Helix.desktop #helix not installed
+#rm -f /usr/share/applications/helix.desktop #helix not installed
 rm -f /usr/share/applications/yazi.desktop
 rm -f /usr/share/applications/nvtop.desktop
-rm -f /etc/xdg/autostart/com.cloudflare.WarpTaskbar.desktop
+#rm -f /etc/xdg/autostart/com.cloudflare.WarpTaskbar.desktop #cloudflare-warp not installed
